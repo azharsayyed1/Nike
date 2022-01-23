6507f9ed3d084c9ca33d4ba976d3c237dd4c9d81
Deepanshu Gulia <deepanshugulia256@gmail.com>
2022-01-23 15:12:00 +0530

    Checkout page
    checkout page is done

--- script/checkout.js
+++ script/checkout.js
@@ -2,6 +2,19 @@
     var payment = document.querySelector("#checkout_payment_div");
     var order = document.querySelector("#checkout_order_status_div");
 
+    const today_date = new Date();
+
+    var order_date = today_date.getDate()+3;
+    var order_month = today_date.getMonth()+1;
+    if(order_date > 28){
+        order_date = 3;
+        order_month = today_date.getMonth()+2;
+    }
+
+    console.log(order_date, order_month,today_date.getFullYear());
+
+    document.querySelector("#you_will_recice_order_on").textContent = "Your Order Will Be delivered till :- "+(order_date+" "+order_month+" "+today_date.getFullYear());
+
 
 
     var userdata = JSON.parse(localStorage.getItem("user_order"));
@@ -55,9 +68,8 @@
     function makepayment(){
         var cardno = document.querySelector("#cardnumber").value;
         var date = document.querySelector("#cardexpirydate").value;
-        var cvv = document.querySelector("#cardcvv").value;
 
-        if(cardno == "" || date == "" || cvv == ""){
+        if(cardno == "" || date == ""){
             alert("Error! Fill all slot");
         }
         else{
@@ -77,5 +89,10 @@
     function cancelorder(){
         setTimeout(()=>{
             alert("Our order is canceled. Countinue shoping");
+            window.location.href = "../index.html";
         }, 2000)
     }
+
+    document.querySelector("#countinue_shopping").addEventListener("click", function(){
+        window.location.href = "../index.html";
+    })
